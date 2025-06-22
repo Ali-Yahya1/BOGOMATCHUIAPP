b3d9d246e3c31ec4ab4faf36ffa0320302be419c
Syed Muhammad Khizer <syed.khizer30@gmail.com>
2025-06-22 22:18:48 +0500

    v0.02

--- src/app/routes/signup/signup.ts
+++ src/app/routes/signup/signup.ts
@@ -2,10 +2,11 @@ import { Component, ChangeDetectionStrategy } from "@angular/core";
 import { RouterLink } from "@angular/router";
 import { Navbar } from "@components/navbar/navbar";
 import { Footer } from "@components/footer/footer";
+import { NgClass } from "@angular/common";
 
 @Component({
   selector: "app-signup",
-  imports: [RouterLink, Footer, Navbar],
+  imports: [RouterLink, Footer, Navbar, NgClass],
   templateUrl: "./signup.html",
   styleUrl: "./signup.css",
   changeDetection: ChangeDetectionStrategy.OnPush
@@ -13,5 +14,15 @@ import { Footer } from "@components/footer/footer";
 
 export class SignUp
 {
+  type: string = 'password';
+  eyeIcon: string = 'fa-eye-slash';
+  isText: boolean = false;
+
+  togglePasswordVisibility()
+  {
+    this.isText = !this.isText;
+    this.isText ? this.eyeIcon = "fa-eye" : this.eyeIcon = "fa-eye-slash";
+    this.isText ? this.type = "text" : this.type = "password";
+  }
 
 }
