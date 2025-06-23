098177d74a531840f6b82e2b567ca78de4075520
Syed Muhammad Khizer <syed.khizer30@gmail.com>
2025-06-23 19:49:45 +0500

    v0.09

--- src/app/routes/signin/signin.ts
+++ src/app/routes/signin/signin.ts
@@ -1,12 +1,13 @@
 import { Component, ChangeDetectionStrategy } from "@angular/core";
 import { RouterLink } from "@angular/router";
+import { FormsModule } from "@angular/forms";
 import { NgClass } from "@angular/common";
 import { Navbar } from "@components/navbar/navbar";
 import { Footer } from "@components/footer/footer";
 
 @Component({
   selector: "app-signin",
-  imports: [RouterLink, Footer, Navbar, NgClass],
+  imports: [RouterLink, Footer, Navbar, NgClass, FormsModule],
   templateUrl: "./signin.html",
   styleUrl: "./signin.css",
   changeDetection: ChangeDetectionStrategy.OnPush
@@ -24,4 +25,7 @@ export class SignIn
     this.isText ? this.eyeIcon = "fa-eye" : this.eyeIcon = "fa-eye-slash";
     this.isText ? this.type = "text" : this.type = "password";
   }
+
+  email: string = "";
+  password: string = "";
 }
